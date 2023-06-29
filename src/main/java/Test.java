f8c77a71264f11b3d851127fb0bed09e89d29be6
Mingwan Kim <tolskais@gmail.com>
2023-06-30 00:31:54 +0900

    Add an example.

---
+++ src/main/java/Test.java
@@ -0,0 +1,17 @@
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+public class Test {
+    public static void main(String... args) throws ConfigurationException {
+        Parameters params = new Parameters();
+        FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+                new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+                        .configure(params.fileBased()
+                                .setPath("t t"));
+
+        builder.getConfiguration();
+    }
+}
